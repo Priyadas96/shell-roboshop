5f86a598d2bdb06b119666217d9372f19ff639c2
Priyadas <mpriyadas4@gmail.com>
2026-04-17 11:56:13 +0530

    roboshop started

--- frontend.sh
+++ frontend.sh
@@ -40,21 +40,21 @@ VALIDATE $? "Enabling Nginx:1.24"
 dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx &>>$LOGS_FILE
+systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx
-VALIDATE $? "Starting nginx"
+VALIDATE $? "Starting Nginx"
 
-rm -rf /usr/share/nginx/html/* &>>$LOGS_FILE
-VALIDATE $? "removing nginx default files"
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VALIDATE $? "Removing default content"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading frontend"
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>$LOGS_FILE
+unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "unzipping frontend"
 
-rm -rf /etc/nginx/nginx.config &>>$LOGS_FILE
+rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
 VALIDATE $? "Remove default nginx conf"
 
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
