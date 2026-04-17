9abdf03d623bd99e2aee86aefab78e243e07bea1
Priyadas <mpriyadas4@gmail.com>
2026-04-17 19:50:27 +0530

    roboshop started

--- shipping.sh
+++ shipping.sh
@@ -22,8 +22,8 @@ else
 	echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-echo "Please enter root password to setup"
-read -s MYSQL_ROOT_PASSWORD
+# echo "Please enter root password to setup"
+# read -s MYSQL_ROOT_PASSWORD
 
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE() {
@@ -77,15 +77,9 @@ VALIDATE $? "Starting Shipping"
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Install MySQL"
 
-mysql -h mysql.daws-sunny.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
-if [ $? -ne 0 ]; then
-	mysql -h mysql.daws-sunny.site -uroot -p$MYSQL_ROOT_PASSWORD </app/db/schema.sql &>>$LOG_FILE
-	mysql -h mysql.daws-sunny.site -uroot -p$MYSQL_ROOT_PASSWORD </app/db/app-user.sql &>>$LOG_FILE
-	mysql -h mysql.daws-sunny.site -uroot -p$MYSQL_ROOT_PASSWORD </app/db/master-data.sql &>>$LOG_FILE
-	VALIDATE $? "Loading data into MySQL"
-else
-	echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
-fi
+mysql -h mysql.daws-sunny.site -uroot -pRoboShop@1 </app/db/schema.sql &>>$LOG_FILE
+mysql -h mysql.daws-sunny.site -uroot -pRoboShop@1 </app/db/app-user.sql &>>$LOG_FILE
+mysql -h mysql.daws-sunny.site -uroot -pRoboShop@1 </app/db/master-data.sql &>>$LOG_FILE
 
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Restart shipping"
