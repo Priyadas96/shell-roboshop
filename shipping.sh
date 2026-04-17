d89f5b2f15ce53bd5fe37e5a905192b80c4c8e9e
Priyadas <mpriyadas4@gmail.com>
2026-04-17 19:10:16 +0530

    roboshop started

--- shipping.sh
+++ shipping.sh
@@ -78,17 +78,17 @@ dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Install MySQL"
 
 mysql -h mysql.daws-sunny.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
-
 if [ $? -ne 0 ]; then
-	mysql -h mysql.daws-sunny.site -u root -p$MYSQL_ROOT_PASSWORD </app/db/schema.sql &>>$LOG_FILE
-	mysql -h mysql.daws-sunny.site -u root -p$MYSQL_ROOT_PASSWORD </app/db/app-user.sql &>>$LOG_FILE
-	mysql -h mysql.daws-sunny.site -u root -p$MYSQL_ROOT_PASSWORD </app/db/master-data.sql &>>$LOG_FILE
+	mysql -h mysql.daws-sunny.site -uroot -p$MYSQL_ROOT_PASSWORD </app/db/schema.sql &>>$LOG_FILE
+	mysql -h mysql.daws-sunny.site -uroot -p$MYSQL_ROOT_PASSWORD </app/db/app-user.sql &>>$LOG_FILE
+	mysql -h mysql.daws-sunny -uroot -p$MYSQL_ROOT_PASSWORD </app/db/master-data.sql &>>$LOG_FILE
+	VALIDATE $? "Loading data into MySQL"
 else
-	echo -e "Data is already loaded into Mysql...$Y Skipping $N"
+	echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
 fi
 
 systemctl restart shipping &>>$LOG_FILE
-VALIDATE $? "restarting shipping service"
+VALIDATE $? "Restart shipping"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME - $START_TIME))
